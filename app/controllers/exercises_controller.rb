7b3b1d00fa84f90b831fd44dfbcd42cba006b28c
井出晶 <badminton.1110@i.softbank.jp>
2022-10-19 11:30:40 +0900

    完成

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,20 +8,20 @@ class ExercisesController < ApplicationController
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop.left_outer_joins
+    @shops = Shop.left_outer_joins(foods: :order_foods).distinct.where(foods: { order_foods: { id: nil } })
   end
 
   def exercise3 
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.order_count
+    @address = Address.joins(:orders).select("addresses.*, count(addresses.id) AS orders_count").group('addresses.id').order('orders_count DESC').first
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
+    @customer = Customer.joins(orders: :foods).select("customers.*, sum(price) AS foods_price_sum").group('customers.id').order('foods_price_sum DESC').first
   end
 end
